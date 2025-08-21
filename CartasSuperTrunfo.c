4429cc46560c575cbf71ccf84685554c72c961f4
Nathan-MDSS <nathanmdss2002@gmail.com>
2025-08-21 00:44:04 +0000

    Reenvio da primeira versão, pois fiz o push errado

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -3,98 +3,71 @@
 int main(){
 
     // Minhas variáveis
-    // variáveis para a carta 1
-    char estado1;
-    char codigo1[10];
-    char nomeDaCidade1[30];
-    int populacao1;
-    float area1;
-    float pib1;
-    int numeroDePontosTuristicos1;
-    float densiPop1;
-    float pibPerCapita1;
-    
-    
+    char estado;
+    char codigo[20];
+    char nomeDaCidade[30];
+    int populacao;
+    float area;
+    float pib;
+    int numeroDePontosTuristicos;
 
-    // variáveis para a carta 2
-    char estado2;
-    char codigo2[10];
-    char nomeDaCidade2[30];
-    int populacao2;
-    float area2;
-    float pib2;
-    int numeroDePontosTuristicos2;
-    float densiPop2;
-    float pibPerCapita2;
-    // Aqui eu optei por usar variáveis separadas para cada carta.
-    // tive que adcionar quebras de linha para melhorar a legibilidade do código.
-
-
-    // a estrutura foi feita sem \n para que a entrada de dados fique na mesma linha da pergunta
-    printf("===Super Trunfo===\n");
-    printf("Cadastre a Carta 1:\n");
-    printf("Digite o estado: \n");
-    scanf(" %c", &estado1); // espaço antes de %c para capturar o caractere corretamente
-    printf("Digite o codigo: \n");
-    scanf("%s", codigo1);
-    printf("Digite o nome da Cidade: \n");
-    scanf("%s", nomeDaCidade1);  // tirei o ponteiro "&" porque como é um array não precisa e estava causando erro
-    printf("Digite a População: \n");
-    scanf("%d", &populacao1);
-    printf("Digite a Área: \n");
-    scanf("%f", &area1);
-    printf("Digite o PIB da Cidade: \n");
-    scanf("%f", &pib1);
-    printf("Digite o Número de Pontos Turísticos: \n");
-    scanf("%d", &numeroDePontosTuristicos1);
-
-    printf("\n");
-
-    printf("Cadastre a Carta 2:\n");
-    printf("Digite o estado: \n");
-    scanf(" %c", &estado2); 
-    printf("Digite o codigo:\n ");
-    scanf("%s", codigo2);
-    printf("Digite o nome da Cidade: \n");
-    scanf("%s", nomeDaCidade2); // tirei o ponteiro "&" porque como é um array não precisa e estava causando erro
-    printf("Digite a População: \n");
-    scanf("%d", &populacao2);
-    printf("Digite a Área: \n");
-    scanf("%f", &area2);
-    printf("Digite o PIB da Cidade:\n");
-    scanf("%f", &pib2);
-    printf("Digite o Número de Pontos Turísticos: \n");
-    scanf("%d", &numeroDePontosTuristicos2);
+    // a estrutura foi feita sem \n para que a entrada de daddos fique na mesma linha da pergunta
+    printf("Carta 1:\n");
+    printf("Digite o estado: ");
+    scanf(" %c", &estado); // espaço antes de %c para capturar o caractere corretamente
+    printf("Digite o codigo: ");
+    scanf("%s", codigo);
+    printf("Digite o nome da Cidade: ");
+    scanf("%s", &nomeDaCidade);
+    printf("Digite a População: ");
+    scanf("%d", &populacao);
+    printf("Digite a Área: ");
+    scanf("%f", &area);
+    printf("Digite o PIB da Cidade: ");
+    scanf("%f", &pib);
+    printf("Digite o Número de Pontos Turísticos: ");
+    scanf("%d", &numeroDePontosTuristicos);
 
     printf("\n");
 
     printf("Carta 1:\n");
-    printf("Estado: %c\n", estado1);
-    printf("Código: %s\n", codigo1);
-    printf("Nome da Cidade: %s\n", nomeDaCidade1);
-    printf("População: %d\n", populacao1);
-    printf("Área: %.2f km²\n", area1);
-    printf("PIB: %.2f bilhões de reais\n", pib1);
-    printf("Numero de Pontos Turísticos: %d\n", numeroDePontosTuristicos1);
-    densiPop1 = populacao1 / area1; // densidade populacional da carta 1
-    printf("Densidade Populacional: %.2f hab/km²\n", densiPop1);
-    pibPerCapita1 = pib1 / populacao1; // PIB per capita da carta 1
-    printf("PIB per Capita: %.2f reais\n", pibPerCapita1);
+    printf("Estado: %c\n", estado);
+    printf("Código: %s\n", codigo );
+    printf("Nome da Cidade: %s\n", nomeDaCidade);
+    printf("População: %d\n", populacao);
+    printf("Área: %.2f km²\n", area);
+    printf("PIB: %.2f bilhões de reais\n", pib);
+    printf("Numero de Pontos Turísticos: %d\n", numeroDePontosTuristicos);
 
     printf("\n");
 
     printf("Carta 2:\n");
-    printf("Estado: %c\n", estado2);
-    printf("Código: %s\n", codigo2);
-    printf("Nome da Cidade: %s\n", nomeDaCidade2);
-    printf("População: %d\n", populacao2);
-    printf("Área: %.2f km²\n", area2);
-    printf("PIB: %.2f bilhões de reais\n", pib2);
-    printf("Numero de Pontos Turísticos: %d\n", numeroDePontosTuristicos2);
-    densiPop2 = populacao2 / area2; // densidade populacional da carta 2
-    printf("Densidade Populacional: %.2f hab/km²\n", densiPop2);
-    pibPerCapita2 = pib2 / populacao2; // PIB per capita da carta 2
-    printf("PIB per Capita: %.2f reais\n", pibPerCapita2);
+    printf("Digite o estado: ");
+    scanf(" %c", &estado); 
+    printf("Digite o codigo: ");
+    scanf("%s", codigo);
+    printf("Digite o nome da Cidade: ");
+    scanf("%s", &nomeDaCidade);
+    printf("Digite a População: ");
+    scanf("%d", &populacao);
+    printf("Digite a Área: ");
+    scanf("%f", &area);
+    printf("Digite o PIB da Cidade: ");
+    scanf("%f", &pib);
+    printf("Digite o Número de Pontos Turísticos: ");
+    scanf("%d", &numeroDePontosTuristicos);
 
+    printf("\n");
+
+    printf("Carta 2:\n");
+    printf("Estado: %c\n", estado);
+    printf("Código: %s\n", codigo );
+    printf("Nome da Cidade: %s\n", nomeDaCidade);
+    printf("População: %d\n", populacao);
+    printf("Área: %.2f km²\n", area);
+    printf("PIB: %.2f bilhões de reais\n", pib);
+    printf("Numero de Pontos Turísticos: %d\n", numeroDePontosTuristicos);
+
+ 
     return 0;
 }
